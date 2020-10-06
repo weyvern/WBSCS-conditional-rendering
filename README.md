fd4a6b3a32e90310d3fc8f012cfa7a490390c3af
Jorge Paul <jserranopaul@gmail.com>
2020-10-06 15:23:57 +0200

    Added comments

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # WBS CS 06.10.20 Demo on conditional rendering + data fetching
 
+Designed for educational purposes
+
+## Clone
+
+```bash
+git clone https://github.com/weyvern/WBSCS-conditional-rendering.git
+```
+
 ## Install
 
 ```bash
--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+// Bring in React core, and useState and useEffect hooks
 import React, { useState, useEffect } from 'react';
 
+// App component starts
 const App = () => {
+  // State for loading component
   const [loading, setLoading] = useState(true);
+  // State for data from API
   const [data, setData] = useState();
+  // State for toggling selected articles
   const [selectedArticle, setSelectedArticle] = useState(null);
-
+  // Stores an article object into selectedArticle
   const handleSelected = el => {
     setSelectedArticle(el);
   };
-
+  // Resets selectedArticle
   const handleDeselect = () => {
     setSelectedArticle(null);
   };
 
+  // Runs when the component loads, gets data from JSONplaceholder API and sets loading to false
   useEffect(() => {
     const getData = async () => {
       const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -23,20 +29,24 @@ const App = () => {
     getData();
   }, []);
 
+  // If selectedArticle is defined, it will return the selected article, else, the list of articles
   return selectedArticle ? (
     <div>
       <h1>{selectedArticle.title}</h1>
+      {/*On click, reset selectedArticle*/}
       <button onClick={() => handleDeselect()}>x</button>
       <p>{selectedArticle.body}</p>
     </div>
   ) : (
     <div>
+      {/*If loading is true, it will return the loading component, else, the list of articles*/}
       {loading ? (
         <div>Loading...</div>
       ) : (
         data.map(el => (
           <li key={el.id}>
             {el.title}
+            {/*On click, store article in state*/}
             <button onClick={() => handleSelected(el)}>+</button>
           </li>
         ))
